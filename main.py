e0d1d871d84ae1193082991984e323d69922446c
Parzival284 <srivatsav284@gmail.com>
2023-07-01 13:18:35 +0530

    added roll dice command

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ hello_replies = ["Hello", "Hey there", "How you doing?", "What's up", "Hi there"
 while True:
     query = input("Say something : ")
     if query != "exit":
-        if query == "hello" or query == "hi":
+        if "hi" in query.lower() or "hello" in query.lower():
             print(random.choice(hello_replies))
+        elif query.lower() == "roll a dice":
+            print("You got",random.randint(0,6),".")
     elif query == "exit":
         print("Bye.")
         break
